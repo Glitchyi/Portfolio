45776c4036839b5814a479504517096dd5036512
Glitchyi <advaithnarayanan8@gmail.com>
2022-12-30 19:52:41 +0530

    vursor and mail

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,6 +8,10 @@
             <a class="flex" href="https://www.instagram.com/advaithnarayanan/">Instagram <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
             <a class="flex" href="https://www.fiverr.com/glitchyi">Fivver <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
             <p>-----------------</p>
+            <div class="text-2xl text-gray-500">
+                <p>Contact me at:</p>
+                <a href="mailto://advaith@glitchy.systems" class=" text-2xl">advaith@glitchy.systems</a>
+            </div>
             
         </div>
 
