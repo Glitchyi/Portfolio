1050e94d1ccc8162c36474520379c5a0d4153938
Glitchyi <advaithnarayanan8@gmail.com>
2022-12-30 18:36:28 +0530

    Init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="grid h-screen place-content-around">
+
+<div class=" flex flex-col justify-start lg:mt-5 xl:justify-evenly lg:flex-row">
+  <div class="cur mx-10 flex flex-col justify-start">
+    <img id="profile" class="cur" src="/profile.gif" alt="Profile Pic">
+  </div>
+  <div class="cur mx-10 flex flex-col justify-start font-gsans">
+    <p class="my-5 font-bold text-4xl lg:text-8xl" id="name" >ADVAITH <br> NARAYANAN</p>
+    <p class="font-bold text-xl lg:text-4xl" id="tagline">Student at MITS |<br> Computer Enthusiast |<br>頭いい</p>
+  </div>
+
+</div>
+</div>
+</template>
+
+
+<script>
+</script>
