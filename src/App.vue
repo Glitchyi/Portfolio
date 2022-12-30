b7f6e4b16621fee09894a1337065a711f82668df
Glitchyi <advaithnarayanan8@gmail.com>
2022-12-30 19:37:38 +0530

    dont touch its working

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
+import HomeVue from './components/Home.vue';
+import AboutVue from './components/About.vue';
 </script>
 
 <template>
-  <div class="grid h-screen place-content-around">
-
-<div class=" flex flex-col justify-start lg:mt-5 xl:justify-evenly lg:flex-row">
-  <div class="cur mx-10 flex flex-col justify-start">
-    <img id="profile" class="cur" src="/profile.gif" alt="Profile Pic">
+  <div class="snapping">
+    <div><HomeVue/></div>
+    <div><AboutVue/></div>
   </div>
-  <div class="cur mx-10 flex flex-col justify-start font-gsans">
-    <p class="my-5 font-bold text-4xl lg:text-8xl" id="name" >ADVAITH <br> NARAYANAN</p>
-    <p class="font-bold text-xl lg:text-4xl" id="tagline">Student at MITS |<br> Computer Enthusiast |<br>頭いい</p>
-  </div>
-
-</div>
-</div>
 </template>
 
-
-<script>
-</script>
---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="back" class="wrapper">
+
+        <div class="grid h-screen place-content-around font-gsans text-5xl sm:text-8xl p-10 text-white">
+            <a class="flex" href="https://github.com/Glitchyi/">Github <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
+            <a class="flex" href="https://www.linkedin.com/in/advaith-narayanan-a72152214/">Linkedin <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
+            <a class="flex" href="https://twitter.com/advaithnarayan">Twitter <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
+            <a class="flex" href="https://www.instagram.com/advaithnarayanan/">Instagram <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
+            <a class="flex" href="https://www.fiverr.com/glitchyi">Fivver <img class="w-10 h-10" src="https://img.icons8.com/windows/96/FFFFFF/external-link-squared.png"/></a>
+            <p>-----------------</p>
+            
+        </div>
+
+    </div>
+</template>
