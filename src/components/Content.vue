dd3162ba4248f1f331553e0139c582430340d61c
Glitchyi <advaithnarayanan8@gmail.com>
2023-07-24 14:12:24 +0530

    oops

---
+++ src/components/Content.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="" style="background: linear-gradient(135deg, #2C0057 0%, #04001D 69.79%);">
+        <InfoVue />
+        <AboutVue />
+    </div>
+</template>
+<script>
+import AboutVue from './About.vue';
+import InfoVue from './Info.vue';
+export default{
+    name: 'content',
+    components:{
+        AboutVue,
+        InfoVue
+    }
+}
+</script>
