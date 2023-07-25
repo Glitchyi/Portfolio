62053039529b224299b0a5d0b2ea8a137ad02d2e
Glitchyi <advaithnarayanan8@gmail.com>
2023-07-25 20:46:33 +0530

    command files

---
+++ warning.cmd
@@ -0,0 +1,11 @@
+@echo off
+cls
+
+echo [101m                                                           [0m
+echo [101m                          Warning                          [0m
+echo [101m                                                           [0m
+echo [102m                                                           [0m
+echo [102m                    NOT THE MAIN BRANCH                    [0m
+echo [102m                                                           [0m
+
+timeout /t 10
